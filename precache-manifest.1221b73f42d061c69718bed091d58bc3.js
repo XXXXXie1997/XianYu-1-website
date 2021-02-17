82c8ff0e10c6d752dd220ec03355b748a290a3d1
XXXXXie1997 <1416849251@qq.com>
2021-02-17 20:25:16 +0800

    deploy

---
+++ precache-manifest.1221b73f42d061c69718bed091d58bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5885c02823c9227adf1a",
+    "url": "/XianYu-1-website/css/app.6fa81d32.css"
+  },
+  {
+    "revision": "a9834c7e2c1377a33de421139d1489dd",
+    "url": "/XianYu-1-website/index.html"
+  },
+  {
+    "revision": "5885c02823c9227adf1a",
+    "url": "/XianYu-1-website/js/app.386b18b4.js"
+  },
+  {
+    "revision": "2aa65bd275661c889d9f",
+    "url": "/XianYu-1-website/js/chunk-vendors.fdac971c.js"
+  },
+  {
+    "revision": "d236ac2271ff6c52e63a858bf7a11cc5",
+    "url": "/XianYu-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/XianYu-1-website/robots.txt"
+  }
+]);
